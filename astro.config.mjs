7ccd72eca083e81ead28b1b69c9c5cc918353c61
DylanSolarteR <dasolarter@udistrital.edu.co>
2025-10-14 23:44:09 -0500

    Agregar archivos de configuración de Docker y ajustes de proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: true,
+    port: 4321,
+  },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+});
