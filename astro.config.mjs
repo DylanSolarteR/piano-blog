24d3a3c036f195460bef367fcfc495aa5147c1de
DylanSolarteR <dasolarter@udistrital.edu.co>
2025-11-19 00:07:06 -0500

    Added Lenis Dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import lenis from "astro-lenis";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: true,
     port: 4321,
   },
+
   vite: {
     server: {
       watch: {
@@ -14,4 +17,6 @@ export default defineConfig({
       },
     },
   },
+
+  integrations: [lenis()],
 });
